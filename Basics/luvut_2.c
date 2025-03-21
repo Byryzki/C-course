af09692a90b0b026a6c4d6239a66f2761c6e5dc5
Byryzki <pyry62@gmail.com>
2025-03-21 20:13:30 +0200

    roots

--- Basics/luvut_2.c
+++ Basics/luvut_2.c
@@ -5,24 +5,35 @@ int main(int argc, char **argv)
 {
     int start = atoi(argv[1]);
     int stop = atoi(argv[2]);
+    int first = 1;
 
     while(start < stop)
     {  
         int worthy = 1;
-        for(int i = 1; i <= argc; i++)
+        int i = 1;
+        for(; i < argc; i++)
         {
+            if(start == atoi(argv[i])){break;}
             if(start % atoi(argv[i]) == 0)
             {
                 worthy = 0;
                 break;
-            }
-            
+            } 
         }
         
-        if(worthy){printf("%d \n", start);}
+        if(worthy)
+        {
+            if(first){
+                printf("%d", start);
+                first = 0;
+            }
+            else{
+                printf(" %d", start);
+            }
+        }
         
         start++;
     }
-
+    if(!first){printf("\n");}
     return 0;
 }

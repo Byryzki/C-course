738c053cbe0e1c87eac0ce99d1873a04f204dcca
Byryzki <pyry62@gmail.com>
2025-07-14 15:59:16 +0300

    geometria (ei läpi)

---
+++ Basics/geometria/include/geometria.h
@@ -0,0 +1,12 @@
+#ifndef GEOMETRIA
+#define GEOMETRIA
+
+#include <stdio.h>
+#include <math.h>
+
+static const long double PII = 3.1415926535897932384L; /*Täytyy olla static, että osaa kopsata muuttujan mainiin*/
+
+long double ympyranAla(double r);
+long double pallonTilavuus(double r);
+
+#endif

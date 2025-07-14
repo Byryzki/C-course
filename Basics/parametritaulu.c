e59d8f15c2f39ef4159f8e2dcd62d686b76a91f7
Byryzki <pyry62@gmail.com>
2025-07-14 10:31:21 +0300

    parametritaulu

--- Basics/parametritaulu.c
+++ Basics/parametritaulu.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 int longest(int amount, char **params)
 {
@@ -16,26 +17,29 @@ int longest(int amount, char **params)
 int main(int argc, char **argv)
 {
     int longst = longest(argc, argv);
-    int width = 9 + longst;
+    int kerrannaisyksikko = floor(log10(argc-1));
+    int width = 7 + kerrannaisyksikko + longst;
 
     int i = 0;
-    for(; i<width+1; i++){printf("#");} printf("\n");
+
+    for(; i<2+kerrannaisyksikko+1+3+longst+2; i++){printf("#");} printf("\n");
 
     for(i=1; i<argc; i++)
     {
         int ii;
+        int numlen = floor(log10(i));
         
-        printf("#%4d", i); printf(" | ");
-        printf("%s", argv[i]);
-        for(ii=7+strlen(argv[i]); ii<width-1; ii++){printf(" ");} printf("#\n");
+        printf("#%*s%d", kerrannaisyksikko+1 - numlen, " ", i);
+        printf(" | %s", argv[i]);
+        printf("%*.s", longst-strlen(argv[i]), " "); printf(" #\n");
 
         if(i != argc-1)
         {
-            printf("#----+-"); for(ii=0; ii<=longst+1;ii++){printf("-");} printf("#\n");
+            printf("#"); for(ii=0; ii<=kerrannaisyksikko+1; ii++){printf("-");} printf("-+-"); for(ii=0; ii<=longst; ii++){printf("-");} printf("#\n");
         }
     }
 
-    for(i=0; i<width+1; i++){printf("#");} printf("\n");
+    for(i=0; i<=width; i++){printf("#");} printf("\n");
 
-    return 0;   //argc:llä pakko kikkailla leveys :/
+    return 0;   
 }

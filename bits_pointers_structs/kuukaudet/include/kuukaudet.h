9d32de0be3e30b072bede25eca2b0fa2fca0d1f1
Byryzki <pyry62@gmail.com>
2025-07-23 18:50:06 +0300

    Kuukaudet korjaus yritys

--- bits_pointers_structs/kuukaudet/include/kuukaudet.h
+++ bits_pointers_structs/kuukaudet/include/kuukaudet.h
@@ -19,8 +19,8 @@ enum kuukaudet
 
 enum maara {KK_LKM = 12};
 
-extern const char *KK_NIMET[KK_LKM];
-extern const char KK_PAIVAT[2][KK_LKM];
+static const char *KK_NIMET[KK_LKM];
+static const char KK_PAIVAT[2][KK_LKM];
 
 int karkausvuosi(int y);
 char kkPituus(const char *monthName, int y);

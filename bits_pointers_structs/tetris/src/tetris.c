b5cd77add9384633c5a6192898876ebe7da91f5c
Byryzki <pyry62@gmail.com>
2025-07-24 12:51:17 +0300

    tetris

--- bits_pointers_structs/tetris/src/tetris.c
+++ bits_pointers_structs/tetris/src/tetris.c
@@ -14,45 +14,31 @@ void paivita(char grid[20][10])
 
     for(row=0; row<20; row++)
     {
-        for(col=0; col<10; col++)
-
-        {
-            copy[row][col] = grid[row][col];
-        }
-    }
-
-    for(row=0; row<20; row++)
-    {
         full = 1;
         for(col=0; col<10; col++)
         {
-            if(grid[row][col] == ' '){
-                full = 0; break;}
+            if(grid[row][col] == ' '){full = 0; break;}
         }
 
         if(full)
         {
             sara = 0;
+
+            for(i=0; i<20; i++)
+            {
+                for(col=0; col<10; col++)
+
+                {
+                    copy[i][col] = grid[i][col];
+                }
+            }
+            
             while(sara <= row)
             {
                 for(i=0; i<10; i++){grid[sara][i] = copy[sara-1][i];}
                 sara++;
             }
             for(i=0; i<10; i++){grid[0][i] = ' ';}
-
-            for(row=0; row<20; row++)   /*näytä*/
-            {
-                for(col=0; col<10; col++)
-                {
-                    printf("%c", copy[row][col]);
-                }
-                printf(" ");
-                for(col=0; col<10; col++)
-                {
-                    printf("%c", grid[row][col]);
-                }
-                printf("\n");
-            }
         }
     }
 }

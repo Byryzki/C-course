015f71e40001fdd402f7b5d7df90c54a548531a4
Byryzki <pyry62@gmail.com>
2025-07-24 14:53:46 +0300

    mjonot

--- memory/mjonot/include/mjonot.h
+++ memory/mjonot/include/mjonot.h
@@ -1,7 +1,9 @@
 #ifndef MOJONOT
 #define MJONOT
 
+#include <stddef.h>
 
+char ** kopioi_mjt(char **strs, size_t n);
 
 
 #endif
--- memory/mjonot/src/mjonot.c
+++ memory/mjonot/src/mjonot.c
@@ -1,4 +1,25 @@
 #include "mjonot.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+char ** kopioi_mjt(char **strs, size_t n)
+{
+    int i;
+    int j;
+    int len;
+
+    char **result = malloc(n*sizeof(char*));
+
+    for(i=0; i<n; i++)
+    {
+        len = strlen(strs[i]);
+        result[i] = malloc(len*sizeof(char)+sizeof(char));  /*leave room for /0*/
+
+        for(j=0; j<=len+1; j++)
+        {
+            result[i][j] = strs[i][j];
+        }
+    }
+    return result;
+}
